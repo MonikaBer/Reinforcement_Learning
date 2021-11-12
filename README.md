f31aecd68d2bc0b56de1bbd939cd2dd7b6a67186
Mateusz Kordowski <mateuszkd67@gmail.com>
2021-11-13 00:01:23 +0100

    instalacja bibliotek i rom

--- README.md
+++ README.md
@@ -4,3 +4,16 @@
 [v1](https://onedrive.live.com/edit.aspx?action=editnew&resid=531E553CA6EE03C2!10911&ithint=file%2cdocx&action=editnew&wdNewAndOpenCt=1635605862040&wdPreviousSession=b34f15e1-7138-4bcd-958e-461fe0fbe4e7&wdOrigin=OFFICECOM-WEB.START.NEW)
 
 [v2](https://onedrive.live.com/edit.aspx?action=editnew&resid=531E553CA6EE03C2!10914&ithint=file%2cdocx&action=editnew&wdNewAndOpenCt=1635607261619&wdPreviousSession=a5ffaf89-1c8c-42b6-a68d-c34ed3e85b84&wdOrigin=OFFICECOM-WEB.START.NEW)
+
+
+sudo apt install cuda-toolkit-11-5
+pip install autorom[accept-rom-license]
+
+Źródła
+https://stackoverflow.com/questions/69442971/error-in-importing-environment-openai-gym
+https://superuser.com/questions/247620/how-to-globally-modify-the-default-pythonpath-sys-path
+
+Inne materiały, które mogą pomóc:
+
+dodać plik zawierający ścieżki, w któych python ma szukać skryptów
+/usr/local/lib/python2.6/dist-packages/site-packages.pth
---
+++ examples/assault-v0.py
@@ -0,0 +1,66 @@
+
+# https://gym.openai.com/envs/Assault-v0/
+
+from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import pyvirtualdisplay
+import reverb
+
+import tensorflow as tf
+
+'''
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
+'''
+
+import gym
+from gym import envs
+
+
+display = pyvirtualdisplay.Display(visible=0, size=(1600, 900)).start()
+
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps = 100  # @param {type:"integer"}
+collect_steps_per_iteration =   1# @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 64  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+def testLoad():
+    env_name = 'Assault-v0'
+    #env_name = 'Asteroids-v0'
+    env = gym.make(env_name)
+    #env = suite_gym.load(env_name)
+
+    env.reset()
+    img = env.render(mode='rgb_array')
+    img = PIL.Image.fromarray(img)
+    plt.imshow(img)
+    plt.show()
+
+testLoad()
