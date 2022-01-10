97cc221683fcc6e933155cf0e16cb9c8ea160dbb
Mateusz Kordowski <mateuszkd67@gmail.com>
2022-01-10 18:12:49 +0100

    skrypt instalacji

---
+++ install.sh
@@ -0,0 +1,13 @@
+pip install dm-sonnet
+pip install trfl
+pip install pyvirtualdisplay
+pip install pyglet
+pip install dm-acme[reverb]
+pip install dm-acme
+pip install dm-acme[tf]
+pip install imageio
+pip install PILLOW
+pip install imageio-ffmpeg
+pip install IPython
+
+sudo apt-get install -y xvfb ffmpeg
