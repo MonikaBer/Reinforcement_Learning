e8f828c27821bf419d97dcdbc51e19afcd9660e8
MonikaBer <berlinska.monika@outlook.com>
2022-01-17 11:22:04 +0100

    fast refactoring

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ from acme.adders import reverb as adders
 from acme.agents.tf import actors as actors
 from acme.datasets import reverb as datasets
 from acme.wrappers import gym_wrapper
-from acme.wrappers import atari_wrapper
 from acme import specs
 from acme import wrappers
 from acme.agents import tf
@@ -19,7 +18,6 @@ from acme.agents import agent
 from acme.tf import utils as tf2_utils
 from acme.utils import loggers
 
-from acme.wrappers.gym_wrapper import GymAtariAdapter
 import gym
 import atari_py
 
@@ -39,12 +37,14 @@ import functools
 
 import pdb
 
+
 FLAGS = {
     'num_episodes': 1
 }
 
 Images = tf.Tensor
 QValues = tf.Tensor
+
 
 class MyDQNAtariNetwork(networks.DQNAtariNetwork):
     def __init__(self, num_actions: int):
@@ -54,7 +54,6 @@ class MyDQNAtariNetwork(networks.DQNAtariNetwork):
         inputs = tf.cast(x = inputs, dtype = tf.float32)
         return self._network(inputs)
 
-
 ##
 ##
 ##
@@ -63,6 +62,7 @@ class MyDQNAtariNetwork(networks.DQNAtariNetwork):
 ##
 ##
 ##
+
 
 def printAttrs(obj):
     print(vars(obj))
@@ -74,6 +74,7 @@ def printAttrs(obj):
     #
     # Z tego wynika, że nie można używać tutaj wrapera od Atari, bo nie ma obiektu 'ale' czyli liczby żyć. Jest jedynie wynik.
     # specs.make_environment_spec(env) to pobiera.
+
 
 def printSpec(env):
     print(getEnvSpec(env))
@@ -90,14 +91,12 @@ def printSpec(env):
 ##  koniec
 ##
 
+
 def createDisplay(x = 160, y = 210):
-	return pyvirtualdisplay.Display(visible=0, size=(x, y)).start()
+	return pyvirtualdisplay.Display(visible = 0, size = (x, y)).start()
+
 
 def createEnv(envName):
-    #genv = gym.make(envName)
-    #env = GymAtariAdapter(genv)
-    #env = wrappers.SinglePrecisionWrapper(env)
-    #return env
     env = gym.make(envName)
 
     #print(env.ale.lives())
@@ -134,11 +133,13 @@ def createEnv(envName):
 def getEnvSpec(env):
     return specs.make_environment_spec(env)
 
+
 def printEnv(envSpec):
     print('actions:\n', envSpec.actions, '\n')
     print('observations:\n', envSpec.observations, '\n')
     print('rewards:\n', envSpec.rewards, '\n')
     print('discounts:\n', envSpec.discounts, '\n')
+
 
 def createAgent(envSpec, explorationSigma = 0.3):
     base = networks.DQNAtariNetwork(envSpec.actions[0].num_values)
@@ -147,12 +148,13 @@ def createAgent(envSpec, explorationSigma = 0.3):
     base = snt.Sequential([tf.keras.layers.Rescaling(1./255, input_shape=(batch_size, img_height, img_width, 3))]) + base
     return acme.agents.tf.dqn.agent.DQN(envSpec, base)
 
-def saveVideo(frames, filename='temp.mp4'):
+
+def saveVideo(frames, filename = 'temp.mp4'):
     if(not isinstance(frames, np.ndarray)):
         frames = np.array(frames)
     """Save and display video."""
     # Write video
-    with imageio.get_writer(filename, fps=60) as video:
+    with imageio.get_writer(filename, fps = 60) as video:
         for frame in frames:
             video.append_data(frame)
     # Read video and display the video
@@ -162,8 +164,10 @@ def saveVideo(frames, filename='temp.mp4'):
                 'src="data:video/mp4;base64,{0}">').format(b64_video.decode())
     return IPython.display.HTML(video_tag)
 
+
 def render(env):
     return env.environment.render(mode = 'rgb_array')
+
 
 def collectFrames(environment, actor, steps = 500):
     frames = []
@@ -175,29 +179,32 @@ def collectFrames(environment, actor, steps = 500):
         timestep = environment.step(action)
     return frames
 
+
 def createServer(env_spec):
     replay_buffer = reverb.Table(
-        name=adders.DEFAULT_PRIORITY_TABLE,
-        max_size=1000000,
-        remover=reverb.selectors.Fifo(),
-        sampler=reverb.selectors.Uniform(),
-        rate_limiter=reverb.rate_limiters.MinSize(min_size_to_sample=1),
+        name = adders.DEFAULT_PRIORITY_TABLE,
+        max_size = 1000000,
+        remover = reverb.selectors.Fifo(),
+        sampler = reverb.selectors.Uniform(),
+        rate_limiter = reverb.rate_limiters.MinSize(min_size_to_sample = 1),
         signature = adders.NStepTransitionAdder.signature(env_spec)
     )
 
-    replay_server = reverb.Server([replay_buffer], port=None)
+    replay_server = reverb.Server([replay_buffer], port = None)
     replay_server_address = 'localhost:%d' % replay_server.port
-
     return replay_server, replay_server_address
+
 
 def createExperienceBuffer(serverAddress):
     adder = adders.NStepTransitionAdder(
-        client=reverb.Client(serverAddress),
-        n_step=5,
-        discount=0.99)
+        client = reverb.Client(serverAddress),
+        n_step = 5,
+        discount = 0.99
+    )
     return adder
 
-def collectExperience(environment, expBuffer, actor, episodes=2):
+
+def collectExperience(environment, expBuffer, actor, episodes = 2):
     for episode in range(episodes):
         timestep = environment.reset()
         expBuffer.add_first(timestep)
@@ -205,65 +212,82 @@ def collectExperience(environment, expBuffer, actor, episodes=2):
         while not timestep.last():
             action = actor.select_action(timestep.observation)
             timestep = environment.step(action)
-            expBuffer.add(action=action, next_timestep=timestep)
+            expBuffer.add(action=action, next_timestep = timestep)
 
 
 def createReporter(serverAddress):
     dataset = iter(datasets.make_dataset(
-        server_address=serverAddress,
-        batch_size=256,
-        transition_adder=True,
-        prefetch_size=4))
+        server_address = serverAddress,
+        batch_size = 256,
+        transition_adder = True,
+        prefetch_size = 4)
+    )
     return dataset
+
 
 def loopAgent(env, agent, episodes):
     loop = acme.EnvironmentLoop(env, agent)
     loop.run(episodes)
 
+
 def rest():
     critic_network = snt.Sequential([
         networks.CriticMultiplexer(
-            observation_network=tf2_utils.batch_concat,
-            action_network=tf.identity,
-            critic_network=networks.LayerNormMLP(
-                layer_sizes=(400, 300),
-                activate_final=True)),
+            observation_network = tf2_utils.batch_concat,
+            action_network = tf.identity,
+            critic_network = networks.LayerNormMLP(
+                layer_sizes = (400, 300),
+                activate_final = True
+            )
+        ),
         # Value-head gives a 51-atomed delta distribution over state-action values.
-        networks.DiscreteValuedHead(vmin=-150., vmax=150., num_atoms=51)])
+        networks.DiscreteValuedHead(vmin = -150., vmax = 150., num_atoms = 51)
+    ])
 
     # Create the target networks
     target_policy_network = copy.deepcopy(policy_network)
     target_critic_network = copy.deepcopy(critic_network)
 
     # We must create the variables in the networks before passing them to learner.
-    tf2_utils.create_variables(network=policy_network,
-                            input_spec=[environment_spec.observations])
-    tf2_utils.create_variables(network=critic_network,
-                            input_spec=[environment_spec.observations,
-                                        environment_spec.actions])
-    tf2_utils.create_variables(network=target_policy_network,
-                            input_spec=[environment_spec.observations])
-    tf2_utils.create_variables(network=target_critic_network,
-                            input_spec=[environment_spec.observations,
-                                        environment_spec.actions])
+    tf2_utils.create_variables(
+        network = policy_network,
+        input_spec = [environment_spec.observations]
+    )
 
-    learner = d4pg.D4PGLearner(policy_network=policy_network,
-                            critic_network=critic_network,
-                            target_policy_network=target_policy_network,
-                            target_critic_network=target_critic_network,
-                            dataset_iterator=dataset,
-                            discount=0.99,
-                            target_update_period=100,
-                            policy_optimizer=snt.optimizers.Adam(1e-4),
-                            critic_optimizer=snt.optimizers.Adam(1e-4),
-                            # Log learner updates to console every 10 seconds.
-                            logger=loggers.TerminalLogger(time_delta=10.),
-                            checkpoint=False)
+    tf2_utils.create_variables(
+        network = critic_network,
+        input_spec = [environment_spec.observations, environment_spec.actions]
+    )
 
-    num_training_episodes =  10 # @param {type: "integer"}
+    tf2_utils.create_variables(
+        network = target_policy_network,
+        input_spec = [environment_spec.observations]
+    )
+
+    tf2_utils.create_variables(
+        network = target_critic_network,
+        input_spec = [environment_spec.observations, environment_spec.actions]
+    )
+
+    learner = d4pg.D4PGLearner(
+        policy_network = policy_network,
+        critic_network = critic_network,
+        target_policy_network = target_policy_network,
+        target_critic_network = target_critic_network,
+        dataset_iterator = dataset,
+        discount = 0.99,
+        target_update_period=100,
+        policy_optimizer = snt.optimizers.Adam(1e-4),
+        critic_optimizer = snt.optimizers.Adam(1e-4),
+        # Log learner updates to console every 10 seconds.
+        logger = loggers.TerminalLogger(time_delta = 10.),
+        checkpoint = False
+    )
+
+    num_training_episodes =  10             # @param {type: "integer"}
     min_actor_steps_before_learning = 1000  # @param {type: "integer"}
-    num_actor_steps_per_iteration =   100 # @param {type: "integer"}
-    num_learner_steps_per_iteration = 1  # @param {type: "integer"}
+    num_actor_steps_per_iteration = 100     # @param {type: "integer"}
+    num_learner_steps_per_iteration = 1     # @param {type: "integer"}
 
     learner_steps_taken = 0
     actor_steps_taken = 0
@@ -279,7 +303,7 @@ def rest():
             next_timestep = environment.step(action)
 
             # Record the transition.
-            actor.observe(action=action, next_timestep=next_timestep)
+            actor.observe(action = action, next_timestep = next_timestep)
 
             # Book-keeping.
             episode_return += next_timestep.reward
@@ -288,31 +312,36 @@ def rest():
 
             # See if we have some learning to do.
             if (actor_steps_taken >= min_actor_steps_before_learning and
-                actor_steps_taken % num_actor_steps_per_iteration == 0):
-            # Learn.
+              actor_steps_taken % num_actor_steps_per_iteration == 0):
+
+                # Learn.
                 for learner_step in range(num_learner_steps_per_iteration):
                     learner.step()
+
             learner_steps_taken += num_learner_steps_per_iteration
 
     # Log quantities.
     print('Episode: %d | Return: %f | Learner steps: %d | Actor steps: %d'%(
-        episode, episode_return, learner_steps_taken, actor_steps_taken))
+            episode, episode_return, learner_steps_taken, actor_steps_taken)
+    )
 
 
-    d4pg_agent = agent.Agent(actor=actor,
-                            learner=learner,
-                            min_observations=1000,
-                            observations_per_step=8.)
+    d4pg_agent = agent.Agent(
+        actor = actor,
+        learner = learner,
+        min_observations = 1000,
+        observations_per_step = 8.
+    )
 
     # This may be necessary if any of the episodes were cancelled above.
     adder.reset()
 
     # We also want to make sure the logger doesn't write to disk because that can
     # cause issues in colab on occasion.
-    logger = loggers.TerminalLogger(time_delta=10.)
+    logger = loggers.TerminalLogger(time_delta = 10.)
 
-    loop = environment_loop.EnvironmentLoop(environment, d4pg_agent, logger=logger)
-    loop.run(num_episodes=50)
+    loop = environment_loop.EnvironmentLoop(environment, d4pg_agent, logger = logger)
+    loop.run(num_episodes = 50)
 
     # Run the actor in the environment for desired number of steps.
     frames = []
@@ -328,6 +357,7 @@ def rest():
     display_video(np.array(frames))
 
     del replay_server
+
 
 def test():
     environment_name = 'Assault-v4'
@@ -351,6 +381,7 @@ def test():
 
     frames = collectFrames(env, agent)
     saveVideo(frames)
+
 
 def run():
     environment_name = 'Assault-v4'
